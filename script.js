6c32771d41f8f0ae47b63030b363e87b9256b9e3
Pasha127 <pasha.account@gmail.com>
2022-07-14 16:24:58 +0200

    squeezing in a bit more

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bodyNode = document.getElementsByTagName('body')[0];
+bodyNode.classList.add('body');
 const headerNode = document.createElement('header');
 console.log(bodyNode);
 bodyNode.prepend(headerNode);
@@ -8,9 +9,11 @@ headerNode.append(titleNode);
 const masterListDivNode = document.createElement('div');
 masterListDivNode.innerText = "box zone";
 headerNode.after(masterListDivNode);
+masterListDivNode.classList.add('boxZone');
 const masterListLength = 76;
 for (i=0; i<masterListLength; i++){  
-    const box = masterListDivNode.append(document.createElement(`box`));
+    const box = document.createElement(`box`)
+    masterListDivNode.append(box);
     box.classList.add('box');
 }
 
